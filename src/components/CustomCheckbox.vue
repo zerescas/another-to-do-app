9e16d3ae7b4ba57f5255d7c73cd438a8ecfe99ac
zerescas <zerescas@gmail.com>
2024-07-21 20:10:17 +0900

    move checkbox from TaskItem.vue to separate CustomCheckbox.vue
    Moved 'custom-checkbox' to a new CustomCheckbox.vue component to make it reusable

---
+++ src/components/CustomCheckbox.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const props = defineProps({
+  checked: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emits = defineEmits(['input']);
+
+function updateChecked(event: Event) {
+  const isChecked = (event.target as HTMLInputElement).checked;
+  emits('input', isChecked);
+}
+</script>
+
+<template>
+  <label class="custom-checkbox">
+    <input
+      type="checkbox"
+      :checked="props.checked"
+      @input="updateChecked"
+    />
+    <span class="checkbox"></span>
+  </label>
+</template>
+
+<style scoped>
+@media (prefers-color-scheme: dark) {
+  .custom-checkbox {
+    --custom-checkbox-accent: var(--item-card-accent);
+  }
+}
+</style>
