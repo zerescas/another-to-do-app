f400674b192aafa418d8e83dc82769c8e64c8551
zerescas <zerescas@gmail.com>
2024-07-22 13:17:08 +0900

    vite.config.ts: disable 'vite-plugin-vue-devtools'
    Disabled "vite-plugin-vue-devtools" in vite.config.ts because some features doesn't work ("Timeline")
    From now it recommended to use the "Vue.js devtools" extension

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
