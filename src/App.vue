28322c638f1b293e91ef19a1c467e812e4407b21
zerescas <zerescas@gmail.com>
2024-07-22 21:31:04 +0900

    implement app-wide view transitions
    App.vue:
      - Implemented app-wide view transitions using "<Transition>"
      - Added the CSS class "scale-fade-x" for use in "<Transition>" as the animation
    
    main.css:
      - Changed "#app" from "display: flex" to "display: grid" to maintain vertical centering of children using "margin: auto 0" and to support view transitions
      - Added "grid-row: 1; grid-column: 1;" to "#app" children as a hack to keep them in place during transition animations

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,24 @@ import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
-  <RouterView />
+  <RouterView v-slot="{ Component }">
+    <Transition name="scale-fade">
+      <component :is="Component"></component>
+    </Transition>
+  </RouterView>
 </template>
 
-<style scoped></style>
+<style scoped>
+.scale-fade-enter-active,
+.scale-fade-leave-active {
+  transition:
+    opacity 0.25s,
+    transform 0.15s;
+}
+
+.scale-fade-enter-from,
+.scale-fade-leave-to {
+  transform: scale(0.9);
+  opacity: 0;
+}
+</style>
