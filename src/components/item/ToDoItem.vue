6d4252e79b14b77b4b977ee4b11f7e851936dd2d
zerescas <zerescas@gmail.com>
2024-07-26 20:54:00 +0900

    ToDoItem.vue: implement settings to control content editability
    /types/todo-item-settings.ts:
      - Created "ToDoItemSettings" interface to define the structure of settings for the "ToDoItem.vue" component
    
    ToDoItem.vue:
      - Added "settings" prop with "ToDoItemSettings" type
      - Implemented control of content editability using the "editableContent" property from "settings"

--- src/components/item/ToDoItem.vue
+++ src/components/item/ToDoItem.vue
@@ -5,11 +5,15 @@ import type { ToDo } from '@/types/todo';
 import type { PropType } from 'vue';
 import '@/assets/item/item-card.css';
 import '@/assets/item/todo/todo-item.css';
+import type { ToDoItemSettings } from '@/types/todo-item-settings';
 
 const props = defineProps({
   toDo: {
     type: Object as PropType<ToDo>,
     required: true,
+  },
+  settings: {
+    type: Object as PropType<ToDoItemSettings>,
   },
 });
 
@@ -31,6 +35,21 @@ function updateToDo(updates: ToDo) {
       @input="updateToDo({ done: $event } as ToDo)"
     />
 
-    <p class="todo-content">{{ toDo.content }}</p>
+    <Textarea
+      v-if="settings?.editableContent"
+      ref="toDoContentRef"
+      class="todo-content"
+      :value="toDo.content"
+      @input="updateToDo({ content: ($event.target as HTMLInputElement).value } as ToDo)"
+      maxlength="200"
+      autoResize
+      rows="1"
+    ></Textarea>
+    <p
+      v-else
+      class="todo-content"
+    >
+      {{ toDo.content }}
+    </p>
   </div>
 </template>
---
+++ src/types/todo-item-settings.ts
@@ -0,0 +1,3 @@
+export interface ToDoItemSettings {
+  editableContent?: boolean;
+}
