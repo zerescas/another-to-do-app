cfcff0dc34bf5a26516ac675aef60f828d489321
zerescas <zerescas@gmail.com>
2024-07-27 15:42:24 +0900

    move pinia init from main.ts to pinia-init.ts

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,7 @@ import PrimeVue from 'primevue/config';
 
 import App from './App.vue';
 import router from './router';
-import { useTaskStore } from './stores/taskStore';
-import { useProjectStore } from './stores/projectStore';
-import type { ToDo } from './types/todo';
-import type { Task } from './types/task';
-import type { Project } from './types/project';
+import { initPiniaStores } from './assets/pinia-init';
 
 const app = createApp(App);
 
@@ -23,61 +19,3 @@ app.use(PrimeVue, {
 initPiniaStores();
 
 app.mount('#app');
-
-function initPiniaStores() {
-  const taskStore = useTaskStore();
-  if (!taskStore.loadStateFromLocalStorage()) {
-    const tasks: Array<ToDo> = [
-      {
-        id: 0,
-        content: 'Learn JavaScript',
-        color: '#C1FEBC',
-        done: true,
-      },
-      {
-        id: 0,
-        content: 'Finish the walkthrough of Elden Ring',
-        color: '#F3BFFB',
-        done: false,
-      },
-    ];
-
-    tasks.forEach((t) => {
-      taskStore.createTask(t as Task);
-    });
-  }
-
-  const projectStore = useProjectStore();
-  if (!projectStore.loadStateFromLocalStorage()) {
-    const projects: Array<ToDo> = [
-      {
-        id: 0,
-        content: "Stars don't twinkle",
-        color: '#ecd786',
-        done: true,
-      },
-      {
-        id: 0,
-        content: "The moon doesn't shine",
-        color: '#a255fe',
-        done: true,
-      },
-      {
-        id: 0,
-        content: "Birds don't sing",
-        color: '#9b1101',
-        done: false,
-      },
-      {
-        id: 0,
-        content: "The wind doesn't blow",
-        color: '#019b70',
-        done: false,
-      },
-    ];
-
-    projects.forEach((p) => {
-      projectStore.createProject(p as Project);
-    });
-  }
-}
