76d87064d21286679536feab8dc89c9b2cb6be1d
zerescas <zerescas@gmail.com>
2024-07-27 19:53:08 +0900

    implement CustomPopup.vue
    Implemented a new "CustomPopup.vue" component for creating popups
    
    - The component includes:
      - Properties:
        - To control its visibility ("isPopupOpened")
        - Apply custom styles ("contentWrapperClass")
        - Set a header title ("headerTitle")
      - Event emitter:
        - 'close-pressed' - to notify a parent component about pressed close button
      - Transition:
        - A fade-zoom transition effect for smooth opening and closing animations

---
+++ src/components/CustomPopup.vue
@@ -0,0 +1,108 @@
+<script setup lang="ts">
+const props = defineProps({
+  isPopupOpened: {
+    type: Boolean,
+    required: true,
+  },
+  contentWrapperClass: {
+    type: String,
+  },
+  headerTitle: {
+    type: String,
+  },
+});
+
+const emits = defineEmits(['close-pressed']);
+
+function pressCloseButton() {
+  emits('close-pressed');
+}
+</script>
+
+<template>
+  <Transition name="fade-zoom">
+    <div
+      class="popup"
+      :class="{ opened: isPopupOpened }"
+      v-show="isPopupOpened"
+    >
+      <div :class="contentWrapperClass">
+        <div class="popup-header">
+          <h2 class="popup-header-title">{{ headerTitle }}</h2>
+          <button
+            class="popup-close-button"
+            @click="pressCloseButton"
+          >
+            X
+          </button>
+        </div>
+
+        <div class="popup-content">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<style scoped>
+/* Transition */
+.fade-zoom-enter-active,
+.fade-zoom-leave-active {
+  transition:
+    opacity 0.3s ease,
+    transform 0.3s ease;
+}
+
+/* Elements */
+.fade-zoom-enter-from,
+.fade-zoom-leave-to {
+  opacity: 0;
+  transform: scale(2);
+}
+
+.popup {
+  backdrop-filter: blur(12px);
+  background-color: #00000010;
+  display: flex;
+  inset: 0;
+  overflow: auto;
+  position: absolute;
+  pointer-events: none;
+  z-index: 1000;
+}
+
+.popup.opened {
+  pointer-events: all;
+}
+
+.popup-header {
+  align-items: center;
+  display: flex;
+  position: relative;
+}
+
+.popup-header-title {
+  flex-grow: 1;
+}
+
+.popup-close-button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  font-size: 24px;
+  font-weight: 500;
+  outline: none;
+  padding: 16px 0 16px 16px;
+}
+
+.popup-content {
+  margin-top: 24px;
+}
+
+@media (prefers-color-scheme: dark) {
+  .popup {
+    background-color: #00000080;
+  }
+}
+</style>
