f8de0aa9bccbdd511d5fc19964e9d9b0d9b8c46b
zerescas <zerescas@gmail.com>
2024-07-27 19:59:43 +0900

    TaskSettingsView.vue: implement project pinning to selected task
    - Implemented feature to pin a project to a selected task
    - Added a list of projects to pin, displayed in <CustomPopup>
    - Placed the project list popup within <Teleport> to move it to the app's root container
    - Implemented logic to pin a project when a project clicked

--- src/views/ToDoSettings/TaskSettingsView.vue
+++ src/views/ToDoSettings/TaskSettingsView.vue
@@ -3,15 +3,21 @@ import { computed, ref, type Ref } from 'vue';
 import { useRoute } from 'vue-router';
 import { useTaskStore } from '@/stores/taskStore';
 import { useProjectStore } from '@/stores/projectStore';
+import { pinTaskToProject } from '@/stores/common/pin-task-to-project';
+import { unpinTaskFromProject } from '@/stores/common/unpin-task-from-project';
 import ToDoItem from '@/components/item/ToDoItem.vue';
 import NavBar from '@/components/NavBar.vue';
 import CustomColorSelector from '@/components/CustomColorSelector.vue';
+import CustomPopup from '@/components/CustomPopup.vue';
 import type { NavBarSettings } from '@/types/navbar-settings';
 import type { Task } from '@/types/task';
+import type { Project } from '@/types/project';
 import '@/assets/todo-settings/common.css';
 
 const route = useRoute();
 const routeItemId = route.params.id;
+
+const isPinToProjectPopupOpened = ref(false);
 
 const taskStore = useTaskStore();
 const projectStore = useProjectStore();
@@ -33,6 +39,18 @@ function updateTaskColor(color: string) {
   if (taskItem) {
     taskStore.updateTask(taskItem?.value.id, { color: color });
   }
+}
+
+function selectProjectInList(project: Project | null) {
+  if (taskItem) {
+    if (project) {
+      pinTaskToProject(taskItem.value, project, taskStore, projectStore);
+    } else {
+      unpinTaskFromProject(taskItem.value, taskStore, projectStore);
+    }
+  }
+
+  isPinToProjectPopupOpened.value = false;
 }
 </script>
 
@@ -62,7 +80,10 @@ function updateTaskColor(color: string) {
 
             <div class="item-card setting-card column-span-2">
               <div class="setting-card-title">Project</div>
-              <div class="setting-card-value setting-card-value--limited">
+              <div
+                class="setting-card-value setting-card-value--limited"
+                @click="isPinToProjectPopupOpened = true"
+              >
                 {{ pinnedToProjectName ?? 'Not pinned' }}
               </div>
             </div>
@@ -74,4 +95,61 @@ function updateTaskColor(color: string) {
 
     <NavBar :settings="navBarSettings" />
   </div>
+
+  <Teleport to="#app">
+    <CustomPopup
+      :isPopupOpened="isPinToProjectPopupOpened"
+      :contentWrapperClass="'app-layout app-layout--centered app-layout--padding'"
+      :headerTitle="'Select a project to pin'"
+      @close-pressed="isPinToProjectPopupOpened = false"
+    >
+      <div class="projects-list">
+        <div class="project-item-wrapper">
+          <div
+            class="item-card"
+            @click="selectProjectInList(null)"
+          >
+            <p class="todo-content">None</p>
+          </div>
+        </div>
+
+        <template
+          v-for="project in projectStore.projects"
+          :key="project.id"
+        >
+          <div class="project-item-wrapper">
+            <div
+              class="item-card item-card--decorated todo-item"
+              :style="`--item-card-accent: ${project.color}`"
+              @click="selectProjectInList(project)"
+            >
+              <p class="todo-content">
+                {{ project.content }}
+              </p>
+            </div>
+          </div>
+        </template>
+      </div>
+    </CustomPopup>
+  </Teleport>
 </template>
+
+<style scoped>
+.projects-list {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+}
+
+.projects-list .project-item-wrapper {
+  transition: margin 0.25s;
+}
+
+.projects-list .project-item-wrapper:hover {
+  margin: 0 24px;
+}
+
+.projects-list .item-card {
+  cursor: pointer;
+}
+</style>
