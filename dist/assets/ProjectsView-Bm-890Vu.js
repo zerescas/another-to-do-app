465fb59fdf18840936033d90396bcf1aa9ae5452
zerescas <zerescas@gmail.com>
2024-07-29 14:00:38 +0900

    update Vite config for GitHub Pages, and build app

--- dist/assets/ProjectsView-Bm-890Vu.js
+++
@@ -1,6 +0,0 @@
-import{T as r}from"./ToDoList-DasZH896.js";import{d as c,g as p,c as n,r as l,a as d,b as i,e as o,f as t,o as u}from"./index-BhN4RD5f.js";import{N as m}from"./NavBar-CH7aeHDl.js";const T={class:"app-layout app-layout--desktop-float"},_={class:"app-layout--content app-layout--scrollable app-layout--padding"},P=c({__name:"ProjectsView",setup(j){const e=p(),s=n(()=>`
-    Projects
-    <sup class="title-superscript">
-      ${e.projects.length}  
-    </sup>
-  `),a=l({color:"projects",customTitle:s,customTitleMode:"html"});return(f,g)=>(u(),d("div",T,[i("div",_,[o(r,{items:t(e).projects,toDoType:"project",onCreateTodo:t(e).createProject,onUpdateTodo:t(e).updateProject,onDeleteTodo:t(e).deleteProject},null,8,["items","onCreateTodo","onUpdateTodo","onDeleteTodo"])]),o(m,{settings:a.value},null,8,["settings"])]))}});export{P as default};
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/another-to-do-app/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
